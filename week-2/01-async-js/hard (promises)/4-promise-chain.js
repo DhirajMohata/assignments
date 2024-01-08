bc9b3171c5a0c4482158abf8640a9a1e0a9c2869
DhirajMohata <dhirajmohata86@gamil.com>
2024-01-09 03:24:06 +0530

    solve

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,49 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve , reject) => {
+        setTimeout(()=>{
+            resolve();
+        } , t*1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve , reject) => {
+        setTimeout(()=>{
+            resolve();
+        } , t*1000);
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve , reject) => {
+        setTimeout(()=>{
+            resolve();
+        } , t*1000);
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    var start = Date.now();
+    return wait1(t1)
+    .then(() => {
+      // console.log("First Promise resolved");
+      // Return the next method that returns a promise.
+      return wait2(t2);
+    })
+    .then(() => {
+      // console.log("Second Promise resolved");
+      return wait3(t3);
+    })
+    .then(() => {
+      // console.log("Third Promise resolved");
+      // Record the end time of the complete operation.
+      const end = Date.now();
+      // Find the time difference and return it.
+      const difference = end - start;
+      return difference;
+    });
 }
 
 module.exports = calculateTime;
